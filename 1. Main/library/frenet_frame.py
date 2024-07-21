4e5b235ebec58cfe9908a001c20a79c260e4576c
Riaan Dowling <riaan@3x4.io>
2024-07-21 16:54:48 +0200

    frenet frame

---
+++ 1. Main/library/frenet_frame.py
@@ -0,0 +1,201 @@
+"""
+frenet frame
+"""
+
+import numpy as np
+from sklearn.decomposition import PCA
+import pandas as pd
+
+
+def frenet_math(
+    bagel_loop_data,
+    window_removed_bagel_loop_data,
+    window_interval_actual_samples,
+    window_interval_extra_samples,
+):
+    """
+    frenet_math
+    """
+    # """
+    # -----------------------------------------------------------------
+    # window_interval_extra_samples data
+    # -----------------------------------------------------------------
+    # """
+
+    bagel_loop_data.reset_index(drop=True, inplace=True)
+    window_removed_bagel_loop_data.reset_index(drop=True, inplace=True)
+    PCA_window_bagel_loop_data = window_removed_bagel_loop_data.head(
+        window_interval_extra_samples
+    )
+    PCA_window_bagel_loop_data.reset_index(drop=True, inplace=True)
+
+    # """
+    # -----------------------------------------------------------------
+    # Pseudo-time bias
+    # -----------------------------------------------------------------
+    # """
+    pt_bias = 10000  # Bias pt to ensure PCA is in the direction of PT
+    x_scaled = (
+        pt_bias * PCA_window_bagel_loop_data["pseudo_time_normal"].values
+    )  # returns a numpy array
+    d = {
+        "pseudo_time_normal": x_scaled,
+        "pca_1": PCA_window_bagel_loop_data.pca_1,
+        "pca_2": PCA_window_bagel_loop_data.pca_2,
+    }
+    df = pd.DataFrame(d)
+    df.reset_index(drop=True, inplace=True)
+
+    # """
+    # -----------------------------------------------------------------
+    # Compute PCA
+    # -----------------------------------------------------------------
+    # """
+
+    from numpy.linalg import eig
+
+    # https://machinelearningmastery.com/calculate-principal-component-analysis-scratch-python/
+    # define a matrix
+    A = np.array([[1, 2], [3, 4], [5, 6]])
+    # calculate the mean of each column
+    M = np.mean(df.iloc[:, 0:3].values.T, axis=1)
+    # center columns by subtracting column means
+    C = df.iloc[:, 0:3].values - M
+    # calculate covariance matrix of centered matrix
+    V = np.cov(C.T)
+    # eigendecomposition of covariance matrix
+    values, vectors = eig(V)
+    P = vectors.T.dot(C.T)
+
+    # Built in PCA
+    pca = PCA(n_components=1)
+    pca.fit(df.iloc[:, 0:3].values)
+
+    # Undo pseudo-time bias
+    nv = pca.components_[0]
+    nv[0] = nv[0] / pt_bias
+    a, b, c = nv
+    normal_vector = (1 / (np.sqrt(a**2 + b**2 + c**2))) * nv
+    covariance_length = nv * 3 * np.sqrt(pca.explained_variance_[0])
+    pca.mean_[0] = pca.mean_[0] / pt_bias
+
+    # """
+    # -----------------------------------------------------------------
+    # Project data onto PC-bias
+    # -----------------------------------------------------------------
+    # """
+    M = np.mean(PCA_window_bagel_loop_data.iloc[:, 0:3].values.T, axis=1)
+    # center columns by subtracting column means
+    C_2 = df.iloc[:, 0:3].values - M
+    # calculate covariance matrix of centered matrix
+    V = np.cov(C.T)
+    # eigendecomposition of covariance matrix
+    values, vectors = eig(V)
+    # project data
+    P = nv.T.dot(C_2.T)
+
+    result_df = pd.DataFrame(P)
+    result_df.columns = ["PC1"]
+    result_df["cell_id_number"] = PCA_window_bagel_loop_data["cell_id_number"].values
+
+    # """
+    # -----------------------------------------------------------------
+    # Window interval slize
+    # -----------------------------------------------------------------
+    # """
+    result_df.reset_index(drop=True, inplace=True)
+    if (300 * covariance_length[0]) > pca.mean_[0]:
+        new_window_bagel_loop_data = result_df.sort_values("PC1", ascending=True)
+        new_window_bagel_loop_data = new_window_bagel_loop_data.head(
+            window_interval_actual_samples
+        )
+    else:
+        new_window_bagel_loop_data = result_df.sort_values("PC1", ascending=False)
+        new_window_bagel_loop_data = new_window_bagel_loop_data.head(
+            window_interval_actual_samples
+        )
+
+    new_window_bagel_loop_data.reset_index(drop=True, inplace=True)
+
+    # Usable data after PCA anlysis
+    window_bagel_loop_data = bagel_loop_data[
+        bagel_loop_data["cell_id_number"].isin(
+            new_window_bagel_loop_data["cell_id_number"].values
+        )
+    ]
+    not_window_bagel_loop_data = bagel_loop_data[
+        ~bagel_loop_data["cell_id_number"].isin(
+            window_bagel_loop_data["cell_id_number"].values
+        )
+    ]
+    return_window_removed_bagel_loop_data = window_removed_bagel_loop_data[
+        ~window_removed_bagel_loop_data["cell_id_number"].isin(
+            window_bagel_loop_data["cell_id_number"].values
+        )
+    ]
+    window_bagel_loop_data.reset_index(drop=True, inplace=True)
+    not_window_bagel_loop_data.reset_index(drop=True, inplace=True)
+
+    # """
+    # -----------------------------------------------------------------
+    # Mean
+    # -----------------------------------------------------------------
+    # """
+
+    mean_window_bagel_loop_data = (
+        window_bagel_loop_data.iloc[:, 0:3].mean(axis=0)
+    ).values
+
+    return (
+        mean_window_bagel_loop_data,
+        normal_vector,
+        covariance_length,
+        window_bagel_loop_data,
+        not_window_bagel_loop_data,
+        return_window_removed_bagel_loop_data,
+    )
+
+
+def frenet_frame_slice(
+    bagel_loop_data,
+    window_removed_bagel_loop_data,
+    window_interval_actual_samples,
+    window_interval_extra_samples,
+    final_window,
+):
+    """
+    frenet_frame_slice
+    """
+
+    print("Plane estiamte start.")
+    if final_window is True:  # Incroperate data that does not make a complete widow
+        window_interval_actual_samples = len(
+            window_removed_bagel_loop_data["pseudo_time_normal"]
+        )
+        window_interval_extra_samples = len(
+            window_removed_bagel_loop_data["pseudo_time_normal"]
+        )
+
+    (
+        mean_window_bagel_loop_data,
+        normal_vector,
+        covariance_length,
+        window_bagel_loop_data,
+        not_window_bagel_loop_data,
+        return_window_removed_bagel_loop_data,
+    ) = frenet_math(
+        bagel_loop_data,
+        window_removed_bagel_loop_data,
+        window_interval_actual_samples,
+        window_interval_extra_samples,
+    )
+
+    print("Plane estiamte end.")
+    return (
+        mean_window_bagel_loop_data,
+        normal_vector,
+        covariance_length,
+        window_bagel_loop_data,
+        not_window_bagel_loop_data,
+        return_window_removed_bagel_loop_data,
+    )
